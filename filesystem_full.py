c9bf013dee433f2424b0a51034c2a1da313bbfbb
Eduardo Amma <eduardoamma@gmail.com>
2021-06-11 17:02:05 -0300

    added sanitization for fsname in fscheck function, switched from os.popen to subproces.Popen to allow list arguments

--- filesystem_full.py
+++ filesystem_full.py
@@ -1,12 +1,23 @@
 import os
+import subprocess
+import re
+class MyException(Exception):
+    pass
 def fscheck(fsname): # organizes contents of /proc/mount in mydict with mountpoint as key, rest as values, if input exists as mountpoint, returns value, else returns false.
     mydict = {}
-    fslist = os.popen('cat /proc/mounts')
-    for i in fslist.readlines():
-        mylist = i.strip().split(' ')
-        mydict[mylist[1]] = mylist[:1] + mylist[2:]
-    if fsname in mydict:
-        return mydict(fsname)
+    if re.match(r"^\/[0-9a-zA-Z_.\/]+", fsname): #REVIEW BETTER SANITIZATION METHOD
+        command = {'cat', '/proc/mounts'}
+        fslist = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+        for i in fslist.communicate():
+            mylist = i.strip().split(' ')
+            mydict[mylist[1]] = mylist[:1] + mylist[2:]
+        if fsname in mydict:
+            return mydict(fsname)
+        else:
+            return False
     else:
-        return False
-#testing github push
+        raise MyException("Provided filesystem path is not valid.")
+
+def inodecheck(fsname):
+    inodecheck_command = ['df', '-i']
+    inodecheck_command.append(fsname)
