6f9a342f093371a658bc3f66e58ef881a7a1400d
Eduardo Amma <eduardoamma@gmail.com>
2021-06-23 15:58:54 -0300

    added usagecheck function, tested fscheck inodecheck and usagecheck in py 2.6 OK

--- filesystem_full.py
+++ filesystem_full.py
@@ -17,18 +17,38 @@ def fscheck(fsname): # organizes contents of /proc/mount in mydict with mountpoi
         else:
           return False
     else:
-        raise MyException("Provided filesystem path is not valid.") #fscheck function tested 23/6 OK
+        raise MyException("Provided filesystem path is not valid.") #fscheck function tested 23/6 OK py2.6
 
-def inodecheck(fsname):
+def inodecheck(fsname): # runs df -i against input fs, parses free inode value and returns true if > 0 or false if <= 0
     command = ['df', '-i']
     if re.match(r"^\/[0-9a-zA-Z_.\/]+", fsname): #REVIEW BETTER SANITIZATION METHOD
         command.append(fsname)
-        inodelist = subprocess.Popen(command stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-        if re.match("^[0-9]+", inodelist.stdout.readlines()[-1].strip()):
-            if inodelist.stdout.readlines()[-1].strip().split()[2] > 0:
-                return True
-            elif inodelist.stdout.readlines()[-1].strip().split()[2] <= 0:
-                return False
-            else
-                raise MyExeception("inodecheck used valuen err")
+        with open(os.devnull, 'w') as devnull:
+            piperead = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=devnull)
+            inodelist = piperead.stdout.readlines()[-1].strip().split()
+            if re.match("^[0-9]+", inodelist[2]):
+                if int(inodelist[2]) > 0:
+                    return True
+                elif int(inodelist[2]) <= 0:
+                    return False
+            else:
+                raise MyExeception("inodecheck free inodes value error.") #inodecheck function tested 23/6 OK py2.6
+    else:
+        raise MyException("Filesystem name failed additional REGEX.")
 
+def usagecheck(fsname): # Verifies whether fs usage is over threshold value or not.
+    if re.match(r"^\/[0-9a-zA-Z_.\/]+", fsname): #REVIEW BETTER SANITIZATION METHOD
+        command = ['df', '-h', fsname]
+        threshold = 85
+        with open(os.devnull, 'w') as devnull:
+            piperead = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=devnull)
+            dflist = piperead.stdout.readlines()[-1].strip().split()
+        usageper = "".join([item for item in dflist if "%" in item]).replace("%","")
+        if int(usageper) >= threshold:
+            return True
+        elif int(usageper) < threshold:
+            return False
+        else:
+            raise MyException("Filesystem usage percentage math error.")
+    else:
+        raise MyException("Filesystem name failed additional REGEX.") #usagecheck function tested 23/6 OK py2.6
